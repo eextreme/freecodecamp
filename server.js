39401bf3303744da128f5aa4f50af4a7183f3405
System <none>
2017-08-18 19:59:32 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -3,37 +3,52 @@ var express = require("express")
 var http = require("http")
 
 var app = express();
-app.set("view", 'view/index.html')
+//app.set("view", 'view/index.html')
 
-app.get("/", function (req, resp) {
+app.get('/', function (req, resp) {
   resp.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/TSM", function(req, resp){
+app.get('/TSM', function(req, resp){
   resp.sendFile(__dirname + '/views/tsm.html')
 })
 
-app.get("/RHPM", function(req, resp){
+app.get('/TSM/:input', function(req, resp){
+  resp.send("Timestamp result: " + req.params)
+})
+
+app.get('/RHPM', function(req, resp){
   resp.sendFile(__dirname + '/views/rhpm.html')
 })
 
-app.get("/USM", function(req, resp){
+app.get('/RHPM/:input', function(req, resp){
+  resp.send("RHPM result: " + req.params)
+})
+
+app.get('/USM', function(req, resp){
   resp.sendFile(__dirname + '/views/usm.html')
 })
 
-app.get("/ISA", function(req, resp){
+app.get('/USM/:input', function(req, resp){
+  resp.send("USM result: " + req.params)
+})
+
+app.get('/ISA', function(req, resp){
   resp.sendFile(__dirname + '/views/isa.html')
 })
 
-app.get("/FMM", function(req, resp){
+app.get('/ISA/:input', function(req, resp){
+  resp.send("ISA result: " + req.params)
+})
+
+app.get('/FMM', function(req, resp){
   resp.sendFile(__dirname + '/views/fmm.html')
 })
 
-app.get('/test',function(req,res){
-    var data = req.params;
-    // console.log(data);
-    res.send("<h1>here is your link</h1>")
-});
+app.get('/FMM/:input', function(req, resp){
+  resp.send("FMM result: " + req.params)
+})
+
 app.use('/',express.static(__dirname+'/public'));
 
 app.listen(process.env.PORT,function(){
