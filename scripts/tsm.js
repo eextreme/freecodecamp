53af354aa831bb078fd2df4e6957cc79b407e187
System <none>
2017-08-23 17:24:37 +0000

    Checkpoint

--- scripts/tsm.js
+++ scripts/tsm.js
@@ -1,37 +1,24 @@
 function getTime(time){
   console.log("Your input is: ")
-  var query = /^[0-9]*?/i
+  var query = /^[0-9]*$/
   var chrono = require('chrono-node')
-  var unix;
-  var date;
+  var parsed = chrono.parseDate(time)
+  var unix, date;
   var ref = {0: "January", 1:"February", 2:"March", 3:"April", 4:"May", 5:"June", 
              6:"July", 7:"August", 8:"September", 9:"October", 10:"November", 11:"December"};
   
-  if (time.match(query))
-    {
-      console.log("UNIX Timestamp Detected")
-      var parsed = new Date(time*1000)
-      
-      unix = time;
-      date = ref[parsed.getMonth()]+" "+parsed.getDate()+", "+parsed.getFullYear()
-      
+  if (parsed){
+    unix = Date.parse(parsed)/1000
+    date = ref[parsed.getMonth()]+" "+parsed.getDate() + ", "+parsed.getFullYear()
+  }
+  else if (time.match(query)){
+      unix = time
+      var temp = new Date(time*1000)
+      date = ref[parsed.getMonth()]+" "+parsed.getDate() + ", "+parsed.getFullYear()
     }
-  else
-    {
-      console.log("Natural Language Timestamp Detected")
-      var parsed = chrono.parse(time)[0].start.date()
-
-      
-      if (parsed){
-        unix = Date.parse(parsed)/1000
-        date = ref[parsed.getMonth()]+" "+parsed.getDate()+", "+parsed.getFullYear()
-        //date = parsed
-      }
-      else
-        {
-          unix = null;
-          date = null;
-        }
+  else {
+      unix = null;
+      date =
     }
   
   return {"unix": unix, "natural": date};
