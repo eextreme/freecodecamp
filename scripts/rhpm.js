2495254fa3b819987f33d7624d3db698572cfbe0
System <none>
2017-08-23 19:24:38 +0000

    Checkpoint

--- scripts/rhpm.js
+++ scripts/rhpm.js
@@ -1,8 +1,9 @@
 function getInfo(info){
-  var regex = /^\((.*)\)$/
+  var regex = /\((.*)\)/
   var ip = info.rawHeaders[9].split(",")
   var lang = info.rawHeaders[29].split(",")
-  var soft = regex.exec(info.rawHeaders[21])[0]
+  //var soft = regex.exec(info.rawHeaders[21])
+  var soft = info.rawHeaders[21]
   return {"ip":ip[0],"lang":lang[0], "soft":soft}
 }
 
