ed49b91b0cda62e030bd94644925cad82a240ad5
System <none>
2017-08-23 20:08:37 +0000

    Checkpoint

--- scripts/usm.js
+++ scripts/usm.js
@@ -1,6 +1,13 @@
 function createShortUrl(fullUrl){
   var MongoClient = require('mongodb').MongoClient
-  if 
-}
+  var db_url = "ds149763.mlab.com:49763/eextreme_db"
+  Mongoclient.connect(db_url,function(db, err){
+    if (err) throw err
+    var collection = db.collection("urls")
+    var query = {shorturl: fullUrl}
+    var doc = collection.findOne(query)
+    return doc.fullurl
+  }
+                      }
 
 module.export = createShortUrl;
