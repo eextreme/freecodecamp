4a54714fbd168ade1e884fa9b8447f2a03faadb9
System <none>
2017-08-23 17:34:30 +0000

    Finished Timestamp Microservice

--- scripts/tsm.js
+++ scripts/tsm.js
@@ -3,25 +3,27 @@ function getTime(time){
   var query = /^[0-9]*$/
   var chrono = require('chrono-node')
   var parsed = chrono.parseDate(time)
-  var unix, date;
+  var unix, date, input;
   var ref = {0: "January", 1:"February", 2:"March", 3:"April", 4:"May", 5:"June", 
              6:"July", 7:"August", 8:"September", 9:"October", 10:"November", 11:"December"};
   
   if (parsed){
+    input = "Natural Date"
     unix = Date.parse(parsed)/1000
     date = ref[parsed.getMonth()]+" "+parsed.getDate() + ", "+parsed.getFullYear()
   }
   else if (time.match(query)){
-      unix = time
-      var temp = new Date(time*1000)
-      date = ref[parsed.getMonth()]+" "+parsed.getDate() + ", "+parsed.getFullYear()
+    var temp = new Date(time*1000)
+    input = "Unix Date Number"
+    unix = temp.getTime()
+    date = ref[temp.getMonth()]+" "+temp.getDate() + ", "+temp.getFullYear()
     }
   else {
       unix = null;
-      date =
+      date = null;
     }
   
-  return {"unix": unix, "natural": date};
+  return {"input":input, "unix": unix, "natural": date};
 }
 
 module.exports = getTime
