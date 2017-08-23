6b80033734c98c68a297f264c6f19d41cfb45f2a
System <none>
2017-08-23 18:52:37 +0000

    Checkpoint

--- scripts/rhpm.js
+++ scripts/rhpm.js
@@ -1,8 +1,8 @@
 function getInfo(info){
-  var ip = info.rawHeaders[9]
-  var lang = info.rawHeaders[29]
-  var soft = info.rawHeaders[21]
-  return {"ip":ip,"lang":lang, "soft":soft}
+  var ip = info.rawHeaders[9].split(",")
+  var lang = info.rawHeaders[29].split(",")
+  var soft = info.rawHeaders[21].split(" ")
+  return {"ip":ip[0],"lang":lang[0], "soft":soft[1]}
 }
 
 module.exports = getInfo 
