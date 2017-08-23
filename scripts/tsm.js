acfdaceebb92c68371ee76511571e081cecc0f86
System <none>
2017-08-23 16:18:37 +0000

    Checkpoint

--- scripts/tsm.js
+++ scripts/tsm.js
@@ -1,9 +1,11 @@
 function getTime(time){
   console.log("Your input is: ")
   var query = /^[0-9]/i
+  var chrono = require('chrono-node')
   if (time.match(query))
     {
       console.log("UNIX Timestamp Detected")
+      chrono.parse
     }
   else
     {
