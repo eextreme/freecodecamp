a4777a37d13782824f6044af3969e7c433f85e8d
System <none>
2017-08-23 19:02:38 +0000

    Checkpoint

--- scripts/rhpm.js
+++ scripts/rhpm.js
@@ -1,8 +1,8 @@
 function getInfo(info){
   var ip = info.rawHeaders[9].split(",")
   var lang = info.rawHeaders[29].split(",")
-  var soft = info.rawHeaders[21].split(" ")
-  return {"ip":ip[0],"lang":lang[0], "soft":soft[1]}
+  var soft = info.rawHeaders[21].match((^\(*\)$))[0]
+  return {"ip":ip[0],"lang":lang[0], "soft":soft}
 }
 
 module.exports = getInfo 
