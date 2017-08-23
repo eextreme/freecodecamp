8d0bda10a56cd808b724ab82c3cb75c6c43a6395
System <none>
2017-08-23 16:51:37 +0000

    Checkpoint

--- scripts/tsm.js
+++ scripts/tsm.js
@@ -4,20 +4,25 @@ function getTime(time){
   var chrono = require('chrono-node')
   var unix;
   var date;
+  var ref = {0: "January", 1:"February", 2:"March", 3:"April", 4:"May", 5:"June", 
+             6:"July", 7:"August", 8:"September", 9:"October", 10:"November", 11:"December"};
   
   if (time.match(query))
     {
       console.log("UNIX Timestamp Detected")
+      var parsed = new Date(time*1000)
       unix = time;
-      date = new Date(time*1000)
+      date = ref[parsed.getMonth()]+" "+parsed.getDay()+", "+parsed.getFullYear()
+      
     }
   else
     {
       console.log("Natural Language Timestamp Detected")
-      unix = chrono.parseDate(time)
-      if (unix){
-        unix = chrono.parseDate(time).getTime()/1000
-        date = chrono.parseDate(time)
+      var parsed = chrono.parseDate(time)
+      
+      if (parsed){
+        unix = parsed.getTime()
+        date = ref[parsed.getMonth()]+" "+parsed.getDay()+", "+parsed.getFullYear()
       }
       else
         {
