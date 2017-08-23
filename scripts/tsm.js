d35b91ea61c635151eb3eaaa7930aee2feaf49d9
System <none>
2017-08-23 16:07:37 +0000

    Checkpoint

--- scripts/tsm.js
+++ scripts/tsm.js
@@ -1,5 +1,15 @@
 function getTime(time){
   console.log("Your input is: ")
+  var query = /^[0-9]/i
+  if (time.match(query))
+    {
+      console.log("UNIX Timestamp Detected")
+    }
+  else
+    {
+      console.log("Natural Language Timestamp Detected")
+      
+    }
 }
 
 module.exports = getTime
