fabb2d9f6577e6e82b61f37439d1193bf6f03a89
System <none>
2017-08-24 17:19:35 +0000

    Checkpoint

--- scripts/usm.js
+++ scripts/usm.js
@@ -1,26 +1,33 @@
 function createShortURL(fullUrl){
   var MongoClient = require('mongodb').MongoClient
   var db_url = "mongodb://tester:tester@ds149763.mlab.com:49763/eextreme_db"
+  var results=[]
   
   MongoClient.connect(db_url, function(err, db){
     if (err) throw "connection failed"
-    
+
     var rand = Math.floor((Math.random() * 100000) + 1);
     
     var urlList = db.collection('urls')
     var query = {shortKey: Number.parseInt(fullUrl)}
-    var doc = urlList.findOne(query, function(err, data){
+    
+    urlList.findOne(query, function(err, data){
       if (data){
         console.log(fullUrl + " " + data['fullurl'])
-        return "The code " + fullUrl + " links to " + data['fullurl']
+        results={type: "redir", link: data['fullurl']}
       }
       else{
         var entry = {shortKey: rand, fullurl:fullUrl}
         urlList.insertOne(entry)
-      console.log(fullUrl+" has been added")
-      return "The code"
+        console.log(fullUrl+" has been added")
+        results={added: rand}
+      }
     })
   })
+  
+  if (results){
+    return results;
+  }
 }
 
 module.exports = createShortURL;
