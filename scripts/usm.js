c95e7f8b2b981598808cf6450bfa33820a4a4ebe
System <none>
2017-08-24 17:43:24 +0000

    Checkpoint

--- scripts/usm.js
+++ scripts/usm.js
@@ -1,7 +1,6 @@
-function createShortURL(fullUrl){
+function createShortURL(fullUrl, callback){
   var MongoClient = require('mongodb').MongoClient
   var db_url = "mongodb://tester:tester@ds149763.mlab.com:49763/eextreme_db"
-  var results=null;
   
   MongoClient.connect(db_url, function(err, db){
     if (err) throw "connection failed"
@@ -14,24 +13,22 @@ function createShortURL(fullUrl){
     urlList.findOne(query, function(err, data){
       if (data){
         console.log(fullUrl + " " + data['fullurl'])
-        results={type: "redir", link: data['fullurl']}
-        justReturn(results)
+        var results={type: "redir", link: data['fullurl']}
+        callback(results)
       }
       else{
         var entry = {shortKey: rand, fullurl:fullUrl}
         urlList.insertOne(entry)
         console.log(fullUrl+" has been added")
-        results={type: "created", shortUrl: "https://substantial-screw.glitch.me/USM/"+rand}
+        var results={type: "created", shortUrl: "https://substantial-screw.glitch.me/USM/"+rand}
         justReturn(results)
       }
     })
-  })
-  
-
+  })  
 }
 
 function justReturn(val){
   return val;
 }
 
-module.exports = createShortURL;
+module.exports = createShortURL
