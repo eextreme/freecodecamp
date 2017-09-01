75a138bcfcee2b075f62038b3552dec00915009e
System <none>
2017-09-01 01:20:00 +0000

    Checkpoint

--- scripts/isa.js
+++ scripts/isa.js
@@ -2,7 +2,15 @@
 
 function getSearchResults(input){
   var url = "https://www.googleapis.com/customsearch/v1"
-  var params = 
+  var key = "AIzaSyDvv4_BfBGozm9DfW2nUrg2wUK5RC4D77Y"
+  var engine = "002023485820325712212:tfka6ekczha"
+  var query = input;
+  
+  var http = require("http")
+  http.get(url+"?key="+key+"&cx="+engine+"&q="+query, function(data){
+  console.log(JSON.stringify(data))
+  })
+  
 }
 
 function getSearchHistory(){
