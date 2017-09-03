4bb9a6b65fd7207577cb5b89cbe795a3ceb76d3d
System <none>
2017-09-03 00:31:52 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -92,8 +92,12 @@ app.get('/FMM', function(req, resp){
   resp.sendFile(__dirname + '/views/fmm.html')
 })
 
-app.get('/FMM/:input', function(req, resp){
-  resp.send("FMM result: " + req.params)
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+app.post('/FMM/file', upload.single('avatar'), function (req, res, next) {
+  
+  // req.file is the `avatar` file 
+  // req.body will hold the text fields, if there were any 
 })
 
 app.use('/',express.static(__dirname+'/public'));
